60abc389829fde79ec4c7956046b94cec4770c42
Kaung Myat Hein <95901297+KgMyat-201@users.noreply.github.com>
2023-01-22 21:33:34 +0630

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -139,3 +139,30 @@ for (let i = 0; i < totalFilterBtn; i++) {
     }
   });
 }
+
+/*=============== Back to top scroll btn ===============*/
+
+let myButton = document.getElementById("back-to-top-btn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+
+window.onscroll = function(){scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) 
+  {
+    myButton.style.display = "block";
+  }
+  else {
+    myButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+
+function topFunction() {
+  
+  document.body.scrollTop = 0;
+
+  document.documentElement.scrollTop = 0;
+}
